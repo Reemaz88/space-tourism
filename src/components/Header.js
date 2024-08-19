7136fb534a01ad00ecfe54025593841fd8515c0d
reemaz al soufe <remoso88@gmail.com>
2024-08-19 23:42:04 +0400

    start with structure for pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/shared/logo.svg';
 
 
@@ -6,13 +7,14 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-      <img src={logo} alt="Space Tourism Logo" />
-    </div>
+        <img src={logo} alt="Space Tourism Logo" />
+      </div>
       <nav className='navigation'>
-        <a href='#'>Home</a>
-        <a href='#'>Destination</a>
-        <a href='#'>Crew</a>
-        <a href='#'>Technology</a>
+        <Link to="/">Home</Link>
+        <Link to="/destination">Destination</Link>
+        <Link to="/crew">Crew</Link>
+        <Link to="/technology">Technology</Link>   
+
       </nav>
     </header>
   );
---
+++ src/pages/CrewPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header'; // Adjust the path if needed
+
+function CrewPage() {
+  return (
+    <div>
+      <Header />
+      {/* Rest of your HomePage content */}
+    </div>
+  );
+}
+
+export default CrewPage;
